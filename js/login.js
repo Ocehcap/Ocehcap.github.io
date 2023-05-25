409b9055999a3cddfa8715afa8f3358f9c9d793f
Ocehcap <pauloypacheco@hotmail.com>
2023-05-26 00:19:23 +0100

    teste

--- js/login.js
+++ js/login.js
@@ -1,34 +1,39 @@
 function handleCredentialResponse(response) {
-         const data = jwt_decode(response.credential)
-         console.log(data)
+  const data = jwt_decode(response.credential);
+  console.log(data);
 
-         fullName.textContent = data.name
-         sub.textContent = data.sub
-         given_name.textContent = data.given_name
-         family_name.textContent = data.family_name
-         email.textContent = data.email
-         verifiedEmail.textContent = data.email_verified
-         picture.setAttribute("src", data.picture)
-        }
+  // Enviar os dados do usuário para guardar.js
+  ListaUserApp.salvarUsuario({
+    fullName: data.name,
+    sub: data.sub,
+    given_name: data.given_name,
+    family_name: data.family_name,
+    email: data.email,
+    verifiedEmail: data.email_verified,
+    picture: data.picture
+  });
 
-        window.onload = function () {
-          google.accounts.id.initialize({
-            client_id: "886063558665-5gnbgl39631a73910h7ptn8mn3mt17gn.apps.googleusercontent.com",
-            callback: handleCredentialResponse
-          });
+  // Restante do código...
+}
 
-          google.accounts.id.renderButton(
-            document.getElementById("buttonDiv"),{ 
-                theme: "filled_black", 
-                size: "large",
-                type: "standard",
-                shape: "pill",
-                text: "signin",
-                locale: "en-US",
-                logo_alignment: "left"
-            }  // customization attributes
-          );
+window.onload = function () {
+  google.accounts.id.initialize({
+    client_id: "886063558665-5gnbgl39631a73910h7ptn8mn3mt17gn.apps.googleusercontent.com",
+    callback: handleCredentialResponse
+  });
 
-          google.accounts.id.prompt(); // also display the One Tap dialog
-        }
+  google.accounts.id.renderButton(
+    document.getElementById("buttonDiv"),{ 
+      theme: "filled_black", 
+      size: "large",
+      type: "standard",
+      shape: "pill",
+      text: "signin",
+      locale: "en-US",
+      logo_alignment: "left"
+    }  // customization attributes
+  );
+
+  google.accounts.id.prompt(); // também exibe o diálogo do One Tap
+}
 
