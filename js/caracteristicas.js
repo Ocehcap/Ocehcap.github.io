16927343d944cb91e7918cfea82cf76ddfb31cf3
Ocehcap <pauloypacheco@hotmail.com>
2023-05-26 18:54:51 +0100

    f

--- js/caracteristicas.js
+++ js/caracteristicas.js
@@ -6,26 +6,10 @@ new Vue({
   methods: {
     openModal(sneakerId) {
       const sneakerSelect = document.getElementById(sneakerId);
-      this.selectedSneaker = sneakerSelect.options[sneakerSelect.selectedIndex].text;
+      this.selectedSneaker = sneakerSelect.value;
     }
-  },
-  mounted() {
-    const self = this;
-    const adicionarPOP1Button = document.getElementById('adicionarPOP1');
-    adicionarPOP1Button.addEventListener('click', function() {
-      self.openModal('sneaker1');
-    });
-
-    const adicionarPOP2Button = document.getElementById('adicionarPOP2');
-    adicionarPOP2Button.addEventListener('click', function() {
-      self.openModal('sneaker2');
-    });
   }
 });
-
-
-
-
 
   
   
