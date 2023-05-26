cfef16ddc08f160fb95f180562c5b0cd746e9499
Ocehcap <pauloypacheco@hotmail.com>
2023-05-26 02:19:52 +0100

    s

--- js/login.js
+++ js/login.js
@@ -1,34 +1,50 @@
 function handleCredentialResponse(response) {
-         const data = jwt_decode(response.credential)
-         console.log(data)
+  const data = jwt_decode(response.credential);
+  console.log(data);
 
-         fullName.textContent = data.name
-         sub.textContent = data.sub
-         given_name.textContent = data.given_name
-         family_name.textContent = data.family_name
-         email.textContent = data.email
-         verifiedEmail.textContent = data.email_verified
-         picture.setAttribute("src", data.picture)
-        }
+  // Verificar se o usuário já existe na lista
+  const userList = getUserListFromLocalStorage();
+  if (!userList.includes(data.email)) {
+    // Adicionar o usuário à lista
+    userList.push(data.email);
+    // Armazenar a lista atualizada no localStorage
+    setUserListToLocalStorage(userList);
+  }
+}
 
-        window.onload = function () {
-          google.accounts.id.initialize({
-            client_id: "886063558665-5gnbgl39631a73910h7ptn8mn3mt17gn.apps.googleusercontent.com",
-            callback: handleCredentialResponse
-          });
+function getUserListFromLocalStorage() {
+  const userListJson = localStorage.getItem('userList');
+  if (userListJson) {
+    return JSON.parse(userListJson);
+  } else {
+    return [];
+  }
+}
 
-          google.accounts.id.renderButton(
-            document.getElementById("buttonDiv"),{ 
-                theme: "filled_black", 
-                size: "large",
-                type: "standard",
-                shape: "pill",
-                text: "signin",
-                locale: "en-US",
-                logo_alignment: "left"
-            }  // customization attributes
-          );
+function setUserListToLocalStorage(userList) {
+  const userListJson = JSON.stringify(userList);
+  localStorage.setItem('userList', userListJson);
+}
 
-          google.accounts.id.prompt(); // also display the One Tap dialog
-        }
+window.onload = function() {
+  google.accounts.id.initialize({
+    client_id: "886063558665-5gnbgl39631a73910h7ptn8mn3mt17gn.apps.googleusercontent.com",
+    callback: handleCredentialResponse
+  });
+
+  google.accounts.id.renderButton(
+    document.getElementById("buttonDiv"), {
+      theme: "filled_black",
+      size: "large",
+      type: "standard",
+      shape: "pill",
+      text: "signin",
+      locale: "en-US",
+      logo_alignment: "left"
+    } // customization attributes
+  );
+
+  google.accounts.id.prompt(); // also display the One Tap dialog
+}
+
 
