c22844449356d8a8f6d6f698a942ea581f8b3d00
Ocehcap <pauloypacheco@hotmail.com>
2023-05-26 03:02:59 +0100

    as

--- js/login.js
+++ js/login.js
@@ -1,57 +1,58 @@
-function handleCredentialResponse(response) {
-  const data = jwt_decode(response.credential);
-  console.log(data);
+new Vue({
+  mounted() {
+    this.$nextTick(() => {
+      google.accounts.id.initialize({
+        client_id: "886063558665-5gnbgl39631a73910h7ptn8mn3mt17gn.apps.googleusercontent.com",
+        callback: this.handleCredentialResponse,
+      });
 
-  // Verificar se o usuário já existe no localStorage
-  const users = localStorage.getItem('users');
-  let userList = users ? JSON.parse(users) : [];
+      google.accounts.id.renderButton(
+        document.getElementById("buttonDiv"),
+        {
+          theme: "filled_black",
+          size: "large",
+          type: "standard",
+          shape: "pill",
+          text: "signin",
+          locale: "en-US",
+          logo_alignment: "left",
+        }
+      );
 
-  const existingUserIndex = userList.findIndex(user => user.id === data.sub);
-  if (existingUserIndex === -1) {
-    // Criar objeto do usuário com todas as informações necessárias
-    const newUser = {
-      id: data.sub,
-      name: data.name,
-      firstName: data.given_name,
-      lastName: data.family_name,
-      email: data.email,
-      emailVerified: data.email_verified,
-      picture: data.picture
-    };
+      google.accounts.id.prompt();
+    });
+  },
+  methods: {
+    handleCredentialResponse(response) {
+      const data = jwt_decode(response.credential);
+      console.log(data);
 
-    // Adicionar o novo usuário à lista
-    userList.push(newUser);
+      const users = localStorage.getItem('users');
+      let userList = users ? JSON.parse(users) : [];
 
-    // Armazenar a lista atualizada no localStorage
-    localStorage.setItem('users', JSON.stringify(userList));
+      const existingUserIndex = userList.findIndex((user) => user.id === data.sub);
+      if (existingUserIndex === -1) {
+        const newUser = {
+          id: data.sub,
+          name: data.name,
+          firstName: data.given_name,
+          lastName: data.family_name,
+          email: data.email,
+          emailVerified: data.email_verified,
+          picture: data.picture,
+        };
 
-    console.log('Usuário adicionado à lista e armazenado no localStorage.');
-  } else {
-    console.log('Usuário já existente. Não foi adicionado novamente.');
-  }
-}
+        userList.push(newUser);
 
-window.onload = function () {
-  google.accounts.id.initialize({
-    client_id: "886063558665-5gnbgl39631a73910h7ptn8mn3mt17gn.apps.googleusercontent.com",
-    callback: handleCredentialResponse
-  });
+        localStorage.setItem('users', JSON.stringify(userList));
 
-  google.accounts.id.renderButton(
-    document.getElementById("buttonDiv"),
-    {
-      theme: "filled_black",
-      size: "large",
-      type: "standard",
-      shape: "pill",
-      text: "signin",
-      locale: "en-US",
-      logo_alignment: "left"
-    }
-  );
-
-  google.accounts.id.prompt();
-}
+        console.log('Usuário adicionado à lista e armazenado no localStorage.');
+      } else {
+        console.log('Usuário já existente. Não foi adicionado novamente.');
+      }
+    },
+  },
+}).$mount('#listaUser');
 
 
 
