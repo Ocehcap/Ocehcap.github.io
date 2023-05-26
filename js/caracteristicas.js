74e0724902184037980d3330b1929af5a6e3e15a
Ocehcap <pauloypacheco@hotmail.com>
2023-05-26 18:26:49 +0100

    dsadas

--- js/caracteristicas.js
+++ js/caracteristicas.js
@@ -7,14 +7,10 @@ new Vue({
     openModal(sneakerId) {
       const sneakerSelect = document.getElementById(sneakerId);
       this.selectedSneaker = sneakerSelect.options[sneakerSelect.selectedIndex].text;
-    },
-    clearModal() {
-      this.selectedSneaker = '';
     }
   },
   mounted() {
     const self = this;
-
     const adicionarPOP1Button = document.getElementById('adicionarPOP1');
     adicionarPOP1Button.addEventListener('click', function() {
       self.openModal('sneaker1');
@@ -24,18 +20,10 @@ new Vue({
     adicionarPOP2Button.addEventListener('click', function() {
       self.openModal('sneaker2');
     });
-
-    const closeButton = document.querySelector('#exampleModal .btn-close');
-    closeButton.addEventListener('click', function() {
-      self.clearModal();
-    });
-
-    const submitButton = document.querySelector('#exampleModal .btn-features');
-    submitButton.addEventListener('click', function() {
-      console.log('Form submitted');
-    });
   }
 });
+
+
 
 
 
