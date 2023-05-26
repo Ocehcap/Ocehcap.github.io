ff7f42833285cc349204d99b200274c1eec234ca
Ocehcap <pauloypacheco@hotmail.com>
2023-05-26 19:18:24 +0100

    asadas

--- js/caracteristicas.js
+++ js/caracteristicas.js
@@ -1,29 +1,27 @@
-const app = new Vue({
-  el: '#exampleModal',
-  data: {
-    selectedSneaker: ''
-  },
-  mounted() {
-    const self = this;
-    const adicionarPOP1Button = document.getElementById('adicionarPOP1');
-    adicionarPOP1Button.addEventListener('click', function() {
-      self.openModal('sneaker1');
-    });
+new Vue({
+    el: '#exampleModal',
+    data: {
+      selectedSneaker: ''
+    },
+    methods: {
+      openModal(sneakerId) {
+        const sneakerSelect = document.getElementById(sneakerId);
+        this.selectedSneaker = sneakerSelect.options[sneakerSelect.selectedIndex].text;
+      }
+    },
+    mounted() {
+      const self = this;
+      const adicionarPOP1Button = document.getElementById('adicionarPOP1');
+      adicionarPOP1Button.addEventListener('click', function() {
+        self.openModal('sneaker1');
+      });
 
-    const adicionarPOP2Button = document.getElementById('adicionarPOP2');
-    adicionarPOP2Button.addEventListener('click', function() {
-      self.openModal('sneaker2');
-    });
-  },
-  methods: {
-    openModal(sneakerId) {
-      const sneakerSelect = document.getElementById(sneakerId);
-      this.selectedSneaker = sneakerSelect.value;
+      const adicionarPOP2Button = document.getElementById('adicionarPOP2');
+      adicionarPOP2Button.addEventListener('click', function() {
+        self.openModal('sneaker2');
+      });
     }
-  }
-});
-
-
+  });
   
   
   
