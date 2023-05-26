cf69fa28804ae163c47a42d1af779505a36454c0
Ocehcap <pauloypacheco@hotmail.com>
2023-05-26 02:30:20 +0100

    a

--- js/login.js
+++ js/login.js
@@ -2,38 +2,44 @@ function handleCredentialResponse(response) {
   const data = jwt_decode(response.credential);
   console.log(data);
 
-  // Verificar se o usuário já existe na lista
-  const userList = getUserListFromLocalStorage();
-  if (!userList.includes(data.email)) {
-    // Adicionar o usuário à lista
-    userList.push(data.email);
+  // Verificar se o usuário já existe no localStorage
+  const users = localStorage.getItem('users');
+  let userList = users ? JSON.parse(users) : [];
+
+  const existingUser = userList.find(user => user.id === data.sub);
+  if (!existingUser) {
+    // Criar objeto do usuário com todas as informações necessárias
+    const newUser = {
+      id: data.sub,
+      name: data.name,
+      firstName: data.given_name,
+      lastName: data.family_name,
+      email: data.email,
+      emailVerified: data.email_verified,
+      picture: data.picture
+    };
+
+    // Adicionar o novo usuário à lista
+    userList.push(newUser);
+
     // Armazenar a lista atualizada no localStorage
-    setUserListToLocalStorage(userList);
-  }
-}
+    localStorage.setItem('users', JSON.stringify(userList));
 
-function getUserListFromLocalStorage() {
-  const userListJson = localStorage.getItem('userList');
-  if (userListJson) {
-    return JSON.parse(userListJson);
+    console.log('Usuário adicionado à lista e armazenado no localStorage.');
   } else {
-    return [];
+    console.log('Usuário já existente. Não foi adicionado novamente.');
   }
 }
 
-function setUserListToLocalStorage(userList) {
-  const userListJson = JSON.stringify(userList);
-  localStorage.setItem('userList', userListJson);
-}
-
-window.onload = function() {
+window.onload = function () {
   google.accounts.id.initialize({
     client_id: "886063558665-5gnbgl39631a73910h7ptn8mn3mt17gn.apps.googleusercontent.com",
     callback: handleCredentialResponse
   });
 
   google.accounts.id.renderButton(
-    document.getElementById("buttonDiv"), {
+    document.getElementById("buttonDiv"),
+    {
       theme: "filled_black",
       size: "large",
       type: "standard",
@@ -41,10 +47,11 @@ window.onload = function() {
       text: "signin",
       locale: "en-US",
       logo_alignment: "left"
-    } // customization attributes
+    }
   );
 
-  google.accounts.id.prompt(); // also display the One Tap dialog
+  google.accounts.id.prompt();
 }
+
 
 
