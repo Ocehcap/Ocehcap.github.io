a19045989efff55834e1530be262d630703cb06a
Ocehcap <pauloypacheco@hotmail.com>
2023-05-26 18:14:20 +0100

    sadasd

--- js/caracteristicas.js
+++ js/caracteristicas.js
@@ -7,37 +7,36 @@ new Vue({
     openModal(sneakerId) {
       const sneakerSelect = document.getElementById(sneakerId);
       this.selectedSneaker = sneakerSelect.options[sneakerSelect.selectedIndex].text;
+    },
+    clearModal() {
+      this.selectedSneaker = '';
     }
   },
   mounted() {
     const self = this;
 
-    // Update the event listener for adicionarPOP1Button
     const adicionarPOP1Button = document.getElementById('adicionarPOP1');
     adicionarPOP1Button.addEventListener('click', function() {
       self.openModal('sneaker1');
     });
 
-    // Update the event listener for adicionarPOP2Button
     const adicionarPOP2Button = document.getElementById('adicionarPOP2');
     adicionarPOP2Button.addEventListener('click', function() {
       self.openModal('sneaker2');
     });
 
-    // Update the event listener for the modal close button
-    const closeButton = document.querySelector('#exampleModal .btn-secondary');
+    const closeButton = document.querySelector('#exampleModal .btn-close');
     closeButton.addEventListener('click', function() {
-      self.selectedSneaker = '';
+      self.clearModal();
     });
 
-    // Update the event listener for the modal Submit button
     const submitButton = document.querySelector('#exampleModal .btn-features');
     submitButton.addEventListener('click', function() {
-      // Add your logic to handle the form submission here
       console.log('Form submitted');
     });
   }
 });
+
 
 
   
