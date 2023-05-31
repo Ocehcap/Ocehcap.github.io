99c681b356b0935383bac435ae01df203ac438b2
Ocehcap <pauloypacheco@hotmail.com>
2023-05-31 13:28:41 +0100

    dsadasdasdas

--- js/logout.js
+++ js/logout.js
@@ -3,3 +3,18 @@ function removerItem() {
     alert('Item removido do localStorage!');
     location.reload(); // Recarrega a página
 }
+
+// Verifica se há um usuário atualmente no localStorage
+function verificarUsuarioAtual() {
+    var currentUser = localStorage.getItem('currentUser');
+    var logoutButton = document.getElementById('logoutButton');
+    
+    if (currentUser) {
+        logoutButton.style.display = 'block'; // Exibe o botão
+    } else {
+        logoutButton.style.display = 'none'; // Esconde o botão
+    }
+}
+
+// Chama a função para verificar o usuário atual ao carregar a página
+window.onload = verificarUsuarioAtual;
