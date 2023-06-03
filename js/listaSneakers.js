5b503ab9f6e6295328f5c30ea8df50e290e4a976
Ocehcap <pauloypacheco@hotmail.com>
2023-06-03 22:41:12 +0100

    dasffasd

--- js/listaSneakers.js
+++ js/listaSneakers.js
@@ -1,53 +1,74 @@
-// Vue instance to handle the sneaker list
 const app = new Vue({
-    el: '#listaSneakers',
-    data: {
-      sneakers: [] // Array to store sneakers
-    },
-    methods: {
-      addSneaker(name, photo) {
-        // Check if a sneaker with the same name already exists in the list
-        const existingSneaker = this.sneakers.find(sneaker => sneaker.name === name);
-        if (existingSneaker) {
+  el: '#listaSneakers',
+  data: {
+      sneakers: [], // Array to store sneakers
+      newSneakerName: '', // Property to store the new sneaker name
+      searchTerm: '' // Property to store the search term
+  },
+  computed: {
+      filteredSneakers() {
+          const term = this.searchTerm.trim().toLowerCase();
+          if (term === '') {
+              return this.sneakers;
+          } else {
+              return this.sneakers.filter(sneaker => sneaker.name.toLowerCase().includes(term));
+          }
+      }
+  },
+  methods: {
+    addSneaker() {
+      const name = this.searchTerm.trim();
+      if (name === '') {
+          console.log('Please enter a sneaker name.');
+          return;
+      }
+  
+      // Check if a sneaker with the same name already exists in the list
+      const existingSneaker = this.sneakers.find(sneaker => sneaker.name === name);
+      if (existingSneaker) {
           console.log('Sneaker already exists in the list.');
           return;
-        }
-  
-        const newSneaker = {
-          name: name,
-          photo: photo
-        };
-        this.sneakers.push(newSneaker);
-        // Update the sneakers in localStorage
-        localStorage.setItem('sneakers', JSON.stringify(this.sneakers));
       }
-    },
-    mounted() {
+  
+      const newSneaker = {
+          name: name
+      };
+      this.sneakers.push(newSneaker);
+      // Update the sneakers in localStorage
+      localStorage.setItem('sneakers', JSON.stringify(this.sneakers));
+  
+      // Clear the input field
+      this.searchTerm = '';
+  },
+      removeSneaker(index) {
+          this.sneakers.splice(index, 1);
+          // Update the sneakers in localStorage
+          localStorage.setItem('sneakers', JSON.stringify(this.sneakers));
+      }
+  },
+  mounted() {
       // Retrieve the existing sneakers from localStorage or initialize an empty array
       const storedSneakers = localStorage.getItem('sneakers');
       this.sneakers = storedSneakers ? JSON.parse(storedSneakers) : [];
-  
+
       // Example sneakers
       const initialSneakers = [
-        {
-          name: 'Nike Dunk Low Medium Curry',
-          photo: './images/nike-dunk-low-medium-curry-dropout-1_700x700.png'
-        },
-        {
-          name: 'Nike Dunk Low University Blue',
-          photo: 'path/to/photo2.jpg'
-        },
-        {
-          name: 'Nike Dunk Low Spartan Green',
-          photo: 'path/to/photo3.jpg'
-        }
+          {
+              name: 'Nike Dunk Low Medium Curry',
+          },
+          {
+              name: 'Nike Dunk Low University Blue',
+          },
+          {
+              name: 'Nike Dunk Low Spartan Green',
+          }
       ];
-  
+
       // Add the example sneakers to the list
       initialSneakers.forEach(sneaker => {
-        this.addSneaker(sneaker.name, sneaker.photo);
+          this.addSneaker(sneaker.name);
       });
-    }
-  });
+  }
+});
   
   
