3a82ea3bebc6b7833ce093ed3989cb8aa8276ca9
Ocehcap <pauloypacheco@hotmail.com>
2023-06-03 16:04:26 +0100

    214242

--- js/scripts.js
+++ js/scripts.js
@@ -13,27 +13,27 @@ new Vue({
   methods: {
     aplicarFiltros() {
       let trocasFiltradas = this.trocas;
-  
+
       if (this.filtroTamanho !== '') {
         trocasFiltradas = trocasFiltradas.filter(troca => troca.userHas.SizeoftheShoe === this.filtroTamanho);
       }
-  
+
       if (this.filtroCaixa !== '') {
         trocasFiltradas = trocasFiltradas.filter(troca => troca.userHas.HasBox === this.filtroCaixa);
       }
-  
+
       if (this.filtroEstado !== '') {
         trocasFiltradas = trocasFiltradas.filter(troca => troca.userHas.StateoftheShoe === this.filtroEstado);
       }
-  
+
       if (this.termoPesquisa !== '') {
         const termoBusca = this.termoPesquisa.toLowerCase();
         trocasFiltradas = trocasFiltradas.filter(troca => troca.userHas.SelectedSneaker.toLowerCase().includes(termoBusca));
       }
-  
+
       // Adicionar condição para filtrar trocas com troca.userWants.user vazio
       trocasFiltradas = trocasFiltradas.filter(troca => troca.userWants.user === null);
-  
+
       this.trocasFiltradas = trocasFiltradas;
     },
     getTrocasFromLocalStorage() {
@@ -46,32 +46,35 @@ new Vue({
     AcceptTrade(index) {
       const storedUser = localStorage.getItem('currentUser');
       const userON = JSON.parse(storedUser);
-    
-      // Atualizar o objeto userWants com as informações do usuário desejado
-      this.trocasFiltradas[index].userWants.user = {
-        id: userON.id,
-        name: userON.name,
-        firstName: userON.firstName,
-        lastName: userON.lastName,
-        email: userON.email,
-        emailVerified: userON.emailVerified,
-        picture: userON.picture
-      };
-    
-      const trocaAtualizada = this.trocasFiltradas[index];
-    
-      // Procurar a troca correspondente no array this.trocas
-      const trocaIndex = this.trocas.findIndex(troca => troca.id === trocaAtualizada.id);
-    
-      if (trocaIndex !== -1) {
-        // Substituir a troca antiga pela troca atualizada
-        this.trocas.splice(trocaIndex, 1, trocaAtualizada);
+      if (userON.id === this.trocasFiltradas[index].userHas.user.id) {
+        // Atualizar o objeto userWants com as informações do usuário desejado
+        this.trocasFiltradas[index].userWants.user = {
+          id: userON.id,
+          name: userON.name,
+          firstName: userON.firstName,
+          lastName: userON.lastName,
+          email: userON.email,
+          emailVerified: userON.emailVerified,
+          picture: userON.picture
+        };
+
+        const trocaAtualizada = this.trocasFiltradas[index];
+
+        // Procurar a troca correspondente no array this.trocas
+        const trocaIndex = this.trocas.findIndex(troca => troca.id === trocaAtualizada.id);
+
+        if (trocaIndex !== -1) {
+          // Substituir a troca antiga pela troca atualizada
+          this.trocas.splice(trocaIndex, 1, trocaAtualizada);
+        }
+
+        // Armazenar as trocas atualizadas no localStorage
+        localStorage.setItem('tradeList', JSON.stringify(this.trocas));
+        window.location.reload();
+      }else{
+        alert('This trade is yours!')
       }
-    
-      // Armazenar as trocas atualizadas no localStorage
-      localStorage.setItem('tradeList', JSON.stringify(this.trocas));
-      window.location.reload();
-    }    
+    }
   },
   created() {
     this.getTrocasFromLocalStorage();
@@ -81,4 +84,3 @@ new Vue({
 
 
 
-  
