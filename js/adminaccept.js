22e75160b64f94e15a88844eeafd8ec08b973572
Ocehcap <pauloypacheco@hotmail.com>
2023-06-03 18:09:23 +0100

    r5325432

--- js/adminaccept.js
+++ js/adminaccept.js
@@ -1,29 +1,61 @@
 new Vue({
     el: '#app', // Use uma ID diferente para a segunda instância do Vue
     data: {
-      filtroPessoa: '',
-      trocas: [],
-      trocasPessoa: [] // Array para armazenar as trocas da pessoa
+        filtroPessoa: '',
+        trocas: [],
+        trocasPessoa: [] // Array para armazenar as trocas da pessoa
     },
     methods: {
-      getTrocasFromLocalStorage() {
-        const tradeList = localStorage.getItem('tradeList');
-  
-        if (tradeList) {
-          this.trocas = JSON.parse(tradeList);
-          this.trocasPessoa = this.trocas;
-          // Filtrar as trocas da pessoa atual
-          const storedUser = localStorage.getItem('currentUser');
-          const userON = JSON.parse(storedUser);
-          const idPessoa = userON.id; // Substitua pelo nome da pessoa atual
-          this.trocasPessoa = this.trocasPessoa.filter(troca => troca.userWants.user !== null);
-          this.trocasPessoa = this.trocasPessoa.filter(troca => troca.userHas.user.id === idPessoa || troca.userWants.user.id === idPessoa);
-          this.trocasPessoa = this.trocasPessoa.filter(troca => troca.accept === false);
+        AcceptTrade(index) {
+            this.trocasPessoa[index].accept = true;
 
+            const trocaAtualizada = this.trocasFiltradas[index];
+
+            // Procurar a troca correspondente no array this.trocas
+            const trocaIndex = this.trocas.findIndex(troca => troca.id === trocaAtualizada.id);
+
+            if (trocaIndex !== -1) {
+                // Substituir a troca antiga pela troca atualizada
+                this.trocas.splice(trocaIndex, 1, trocaAtualizada);
+            }
+
+            // Armazenar as trocas atualizadas no localStorage
+            localStorage.setItem('tradeList', JSON.stringify(this.trocas));
+            window.location.reload();
+        },
+        RefuseTrade(index) {
+            const trocaAtualizada = this.trocasFiltradas[index];
+        
+            // Procurar a troca correspondente no array this.trocas
+            const trocaIndex = this.trocas.findIndex(troca => troca.id === trocaAtualizada.id);
+        
+            if (trocaIndex !== -1) {
+                // Remover a troca atualizada do array this.trocas
+                this.trocas.splice(trocaIndex, 1);
+            }
+        
+            // Armazenar as trocas atualizadas no localStorage
+            localStorage.setItem('tradeList', JSON.stringify(this.trocas));
+            window.location.reload();
+        },
+        getTrocasFromLocalStorage() {
+            const tradeList = localStorage.getItem('tradeList');
+
+            if (tradeList) {
+                this.trocas = JSON.parse(tradeList);
+                this.trocasPessoa = this.trocas;
+                // Filtrar as trocas da pessoa atual
+                const storedUser = localStorage.getItem('currentUser');
+                const userON = JSON.parse(storedUser);
+                const idPessoa = userON.id; // Substitua pelo nome da pessoa atual
+                this.trocasPessoa = this.trocasPessoa.filter(troca => troca.userWants.user !== null);
+                this.trocasPessoa = this.trocasPessoa.filter(troca => troca.userHas.user.id === idPessoa || troca.userWants.user.id === idPessoa);
+                this.trocasPessoa = this.trocasPessoa.filter(troca => troca.accept === false);
+
+            }
         }
-      }
     },
     created() {
-      this.getTrocasFromLocalStorage();
+        this.getTrocasFromLocalStorage();
     }
-  });
+});
