3af1d226f79e0efe117746c74070a7c17d996ff0
Ocehcap <pauloypacheco@hotmail.com>
2023-06-03 22:52:31 +0100

    final

--- js/logout.js
+++ js/logout.js
@@ -11,6 +11,7 @@ function verificarUsuarioAtual() {
     var logoutButton = document.getElementById('logoutButton');
     var iconPerf = document.getElementById('iconPerf');
     var PermAdmin = document.getElementById('PermAdmin');
+    var PermAdmin1 = document.getElementById("PermAdmin1");
     var TradeNav = document.getElementById('TradeNav');
     var TradeFeed = document.getElementById('TradeFeed');
     
@@ -18,16 +19,19 @@ function verificarUsuarioAtual() {
         logoutButton.style.display = 'block'; // Exibe o botão
         iconPerf.style.display ='block';
         PermAdmin.style.display = 'none';
+        PermAdmin1.style.display = 'none';
     }else if(currentUser && userON.email === "pauloypacheco25@gmail.com"){
         logoutButton.style.display = 'block'; // Exibe o botão
         iconPerf.style.display ='block';
         PermAdmin.style.display = 'block';
+        PermAdmin1.style.display = 'block';
         TradeNav.style.display = 'block';
         TradeFeed.style.display = 'block';
     }else{
         logoutButton.style.display = 'none'; // Esconde o botão
         iconPerf.style.display = 'none';
         PermAdmin.style.display = 'none';
+        PermAdmin1.style.display = 'none';
         TradeNav.style.display = 'none';
         TradeFeed.style.display = 'none';
     }
